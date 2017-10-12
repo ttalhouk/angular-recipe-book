3c6c423aea70781ae781c98216c939d415585e13
Talal Talhouk <talal@sbcglobal.net>
2017-10-12 11:26:18 -0700

    update to use routing

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -13,9 +13,7 @@ export class RecipesComponent implements OnInit {
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe((recipe:Recipe) => {
-      this.selectedRecipe = recipe
-    })
+
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,12 @@
 import { EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs/Subject'
 
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  // ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+  ingredientsChanged = new Subject<Ingredient[]>();
+
   private ingredients: Ingredient[] = [
     new Ingredient('salt', 10),
     new Ingredient('tomato', 5)
@@ -14,12 +18,12 @@ export class ShoppingListService {
 
   addNewIngredient(ingredient){
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addToList(ingredients){
     this.ingredients = [...this.ingredients, ...ingredients];
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
 }
